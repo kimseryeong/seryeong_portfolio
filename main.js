4d81ad0080e1d319413ccbb6672b7576a8f281f7
kimseryeong <kxxoo322@gmail.com>
2024-01-04 17:47:44 +0900

    커밋이염

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    fnSkill();
+})
+
+// 스킬 프로그레스바
+function fnSkill(){
+    var skillName = [];
+    $('.skill-title').each(function(idx){
+        var $this = $(this).text();
+        skillName.push($this);
+
+        var backColor;
+        var pointColor;
+    })
+}
