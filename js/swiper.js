84c5241a980f4c118f8d3513291bb1b50fd7c2e8
kimseryeong <kxxoo322@gmail.com>
2024-01-15 17:35:00 +0900

    0115 window

--- js/swiper.js
+++ js/swiper.js
@@ -22,17 +22,21 @@ const mySwiper = new Swiper('.swiper-container', {
     //옵션설정
     direction: 'horizontal' //수평(수직은 vertical, 기본값:horizontal)
     ,loop: true //슬라이드 무한 루프(기본값:true)
+    ,loopAdditionalSlides: 1
     ,autoplay: {
         delay: 8000 //자동재생(기본값:false, delay:딜레이 시간)
     }
     ,speed: 500 //슬라이드 애니메이션 속도(기본값:300)
-    ,slidesPerView: 2 //한번에 보여지는 슬라이드 개수 (auto->슬라이드 넓이,높이에 따라 자동 계산)
+    ,slidesPerView: 'auto' //한번에 보여지는 슬라이드 개수 (auto->슬라이드 넓이,높이에 따라 자동 계산)
     ,pagination:{
         el: '.swiper-pagination'
         ,type: 'bullets'
+        ,clickable: true
     }
     ,navigation: {
-        nextEl: '.swiper-btn-next'
-        ,prevEl: '.swiper-btn-prev'
+        nextEl: '.swiper-button-next'
+        ,prevEl: '.swiper-button-prev'
     }
+    ,spaceBetween: 10
+    
 });
