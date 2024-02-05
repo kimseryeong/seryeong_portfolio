73ca3046ffbeb62176d353aceefd32458f696007
kimseryeong <kxxoo322@gmail.com>
2024-02-05 17:34:43 +0900

    timeline

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ $(document).ready(function(){
     //프로젝트 슬라이드
     //fnSlide();
     fnPjtDesc();
+
+    init();
 });
 
 //메뉴 스크롤
@@ -16,9 +18,34 @@ function fnMenuScroll(){
         //console.log($(this).text(), '클릭됨')
         // e.preventDefault();
         $('html, body').animate({scrollTop: $(this.hash).offset.top}, '400'); //$(this.hash).offset().top
+        
+        //$(this).addClass('on');
+        //$(this).css('color', 'red');
     });
 
 }
+//메뉴 클릭 시 클래스추가
+var scrollList = document.getElementsByClassName('scroll');
+function init() {
+    for (var i = 0; i < scrollList.length; i++) {
+        scrollList[i].addEventListener("click", handleClick);
+    }
+}
+function handleClick(e){
+    console.log('e className : ', e.target.classList);
+    if(e.target.classList[1] === 'on'){
+        e.target.classList.remove('on');
+    }
+    else{
+        for (var i = 0; i < scrollList.length; i++) {
+            scrollList[i].classList.remove("on");
+        }
+    
+        e.target.classList.add("on");
+    }
+}
+
+
 
 //맨위로 스크롤
 function fnTopScroll(){
