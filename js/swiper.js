5426dd5b9f6d7816e2c2c28d9d64fe9fc2363c23
kimseryeong <kxxoo322@gmail.com>
2024-02-06 18:00:47 +0900

    resume

--- js/swiper.js
+++ js/swiper.js
@@ -7,13 +7,13 @@ const mySwiper = new Swiper('.swiper-container', {
     ,autoplay: {
         delay: 20000 //자동재생(기본값:false, delay:딜레이 시간)
     }
-    ,speed: 500 //슬라이드 애니메이션 속도(기본값:300)
+    ,speed: 1000 //슬라이드 애니메이션 속도(기본값:300)
     ,slidesPerView: 3 //한번에 보여지는 슬라이드 개수 (auto->슬라이드 넓이,높이에 따라 자동 계산)
-    // ,pagination:{
-    //     el: '.swiper-pagination'
-    //     ,type: 'bullets'
-    //     ,clickable: true
-    // }
+    ,pagination:{
+        el: '.swiper-pagination'
+        ,type: 'bullets'
+        ,clickable: true
+    }
     ,navigation: {
         nextEl: '.swiper-button-next'
         ,prevEl: '.swiper-button-prev'
